03b5c00f3715fb8f27f57abcffede2aae343e13e
stassie-lzrv <adlazareva@edu.hse.ru>
2023-07-06 15:04:47 +0300

    URLSession Extension for HW5

---
+++ ToDoList/Service/Service.swift
@@ -0,0 +1,32 @@
+//
+//  Service.swift
+//  ToDoList
+//
+//  Created by Настя Лазарева on 06.07.2023.
+//
+
+import Foundation
+
+extension URLSession {
+    func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: urlRequest) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.unknown))
+                }
+            }
+            if Task.isCancelled == true {
+                
+                task.cancel()
+                
+            } else {
+                
+                task.resume()
+            }
+        }
+    }
+}
