fd3808bd441a4ac4e1677d11a1556a1ece286093
stassie-lzrv <adlazareva@edu.hse.ru>
2023-07-08 07:42:34 +0300

    HW6*

--- ToDoList/Service/Service.swift
+++ ToDoList/Service/Service.swift
@@ -17,13 +17,32 @@ protocol NetworkService {
 }
 
 final class Service: NetworkService {
+    
+    private var retryCount = 0
+    private let maxRetryCount = 5
+    private var fetchRequestStart = false
+    
+    private let minDelay: Double = 2
+    private let maxDelay: Double = 120
+    private let factor: Double = 1.5
+    private let jitter: Double = 0.05
+    private var currentDelay: Double = 2
+    
+    
     private var revision: Int?
     private let decoder = JSONDecoder()
     private let encoder = JSONEncoder()
     
     func getAll() async throws -> [TodoItem] {
         let url = try RequestProcessor.makeURL()
-        let (data, _) = try await RequestProcessor.performRequest(with: url)
+        let (data, response) = try await RequestProcessor.performRequest(with: url)
+        if response.isSuccessful{
+            self.retryCount = 0
+            self.resetDelay()
+        }else{
+            self.retryCount += 1
+            self.retryRequest {}
+        }
         let networkListToDoItems = try decoder.decode(ListToDoItems.self, from: data)
         revision = networkListToDoItems.revision
         return networkListToDoItems.list.map { TodoItem.convert(from: $0) }
@@ -33,7 +52,14 @@ final class Service: NetworkService {
         let taskURL = try RequestProcessor.makeURL()
         let taskList = ListToDoItems(list: toDoItems.map(\.networkItem))
         let taskHttpBody = try encoder.encode(taskList)
-        let (data, _) = try await RequestProcessor.performRequest(with: taskURL, method: .patch, revision: revision ?? 0, httpBody: taskHttpBody)
+        let (data, response) = try await RequestProcessor.performRequest(with: taskURL, method: .patch, revision: revision ?? 0, httpBody: taskHttpBody)
+        if response.isSuccessful{
+            self.retryCount = 0
+            self.resetDelay()
+        }else{
+            self.retryCount += 1
+            self.retryRequest {}
+        }
         let taskNetwork = try decoder.decode(ListToDoItems.self, from: data)
         revision = taskNetwork.revision
         return taskNetwork.list.map{TodoItem.convert(from: $0)}
@@ -41,14 +67,28 @@ final class Service: NetworkService {
     
     func getTask(toDoItem: TodoItem) async throws {
         let url = try RequestProcessor.makeURL(from: toDoItem.id)
-        let (data, _) = try await RequestProcessor.performRequest(with: url)
+        let (data, response) = try await RequestProcessor.performRequest(with: url)
+        if response.isSuccessful{
+            self.retryCount = 0
+            self.resetDelay()
+        }else{
+            self.retryCount += 1
+            self.retryRequest {}
+        }
         let toDoItemNetwork = try decoder.decode(ElementToDoItem.self, from: data)
         revision = toDoItemNetwork.revision
     }
     
     func deleteRask(toDoItem: TodoItem) async throws {
         let url = try RequestProcessor.makeURL(from: toDoItem.id)
-        let (data, _) = try await RequestProcessor.performRequest(with: url, method: .delete, revision: revision)
+        let (data, response) = try await RequestProcessor.performRequest(with: url, method: .delete, revision: revision)
+        if response.isSuccessful{
+            self.retryCount = 0
+            self.resetDelay()
+        }else{
+            self.retryCount += 1
+            self.retryRequest {}
+        }
         let toDoItemNetwork = try decoder.decode(ElementToDoItem.self, from: data)
         revision = toDoItemNetwork.revision
     }
@@ -57,7 +97,14 @@ final class Service: NetworkService {
         let url = try RequestProcessor.makeURL(from: toDoItem.id)
         let elementToDoItem = ElementToDoItem(element: toDoItem.networkItem)
         let httpBody = try encoder.encode(elementToDoItem)
-        let (responseData, _) = try await RequestProcessor.performRequest(with: url, method: .put, revision: revision, httpBody: httpBody)
+        let (responseData, response) = try await RequestProcessor.performRequest(with: url, method: .put, revision: revision, httpBody: httpBody)
+        if response.isSuccessful{
+            self.retryCount = 0
+            self.resetDelay()
+        }else{
+            self.retryCount += 1
+            self.retryRequest {}
+        }
         let toDoItemNetwork = try decoder.decode(ElementToDoItem.self, from: responseData)
         revision = toDoItemNetwork.revision
     }
@@ -66,8 +113,34 @@ final class Service: NetworkService {
         let elementToDoItem = ElementToDoItem(element: toDoItem.networkItem)
         let url = try RequestProcessor.makeURL()
         let httpBody = try encoder.encode(elementToDoItem)
-        let (responseData, _) = try await RequestProcessor.performRequest(with: url, method: .post, revision: revision, httpBody: httpBody)
+        let (responseData, response) = try await RequestProcessor.performRequest(with: url, method: .post, revision: revision, httpBody: httpBody)
+        if response.isSuccessful{
+            self.retryCount = 0
+            self.resetDelay()
+        }else{
+            self.retryCount += 1
+            self.retryRequest {}
+        }
         let toDoItemNetwork = try decoder.decode(ElementToDoItem.self, from: responseData)
         revision = toDoItemNetwork.revision
+    }
+}
+
+extension Service {
+    
+    private func retryRequest(_ request: @escaping @Sendable () -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + currentDelay) {
+            request()
+        }
+        updateDelay()
+    }
+    
+    private func updateDelay() {
+        let retryValue = Double.random(in: 0...(jitter * currentDelay))
+        currentDelay = min(maxDelay, factor * currentDelay) + retryValue
+    }
+    
+    private func resetDelay() {
+        currentDelay = minDelay
     }
 }
